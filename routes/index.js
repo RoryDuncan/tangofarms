e9d0aeedc496a55129fc9289392a4a2900437850
Rory Duncan <robust.rory@gmail.com>
2014-11-12 14:00:51 -0800

    Add contact page.

--- routes/index.js
+++ routes/index.js
@@ -10,5 +10,13 @@ router.get('/products', function(req, res) {
   res.render('products', { title: 'Our Products - Tango Farms' });
 });
 
+router.get('/training', function(req, res) {
+  res.render('training', { title: 'Dogs - Tango Farms' });
+});
+
+router.get('/contact', function(req, res) {
+  res.render('contact', { title: 'Contact Us - Tango Farms' });
+});
+
 
 module.exports = router;
